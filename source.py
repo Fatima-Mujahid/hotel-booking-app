dff86ed3ee869fcb0808cac8fff422c9931f3a84
Fatima-Mujahid <fmujahid.bese19seecs@seecs.edu.pk>
2022-02-20 11:50:28 +0500

    validate cnic photocopy input

--- source.py
+++ source.py
@@ -1,5 +1,6 @@
-import datetime
 import re
+import datetime
+import os
 
 class HotelBookingApp:
 
@@ -14,7 +15,7 @@ class HotelBookingApp:
         self.rating = 0
           
     def get_num_of_days(self):
-        self.num_of_days = input("Enter number of days (1 to 7): ")
+        self.num_of_days = input("\nEnter number of days (1 to 7): ")
         try: 
             self.num_of_days = int(self.num_of_days)
             if (self.num_of_days < 1 or self.num_of_days > 7):
@@ -24,7 +25,7 @@ class HotelBookingApp:
             print("INVALID INPUT!\nNumber of days must be a valid integer.")
     
     def get_amount(self):
-        self.amount = input("Enter amount to pay per day: ")
+        self.amount = input("\nEnter amount to pay per day: ")
         try: 
             self.amount = int(self.amount)
             if (self.amount < 0):
@@ -42,27 +43,32 @@ class HotelBookingApp:
             print("INVALID INPUT!\nAmount must be a valid integer.")
 
     def get_user_info(self):
-        self.name = input("Enter name: ")
+        self.name = input("\nEnter name: ")
         pattern = re.compile("^[a-zA-Z ]+$")
         if (not(pattern.match(self.name) and self.name.istitle())):
             print("INVALID INPUT!\nName must be alphabetic and in title case.")
 
-        self.birthday = input("Enter birthday (dd-mm-yyyy): ")
+        self.birthday = input("\nEnter birthday (dd-mm-yyyy): ")
         try:
             datetime.datetime.strptime(self.birthday, "%d-%m-%Y")
 
         except ValueError:
             print("INVALID INPUT!\nDate should be valid and in format dd-mm-yyyy.")
 
-        self.cnic = input("Enter CNIC number: ")
+        self.cnic = input("\nEnter CNIC number: ")
         if (not(len(self.cnic) == 13 and re.match('^[0-9]*$', self.cnic))):
             print("INVALID INPUT!\nCNIC number must be numeric and exactly 13 digits.")
 
     def get_cnic_photocopy(self):
-        self.cnic_photocopy = input("Enter absolute path of cnic photocopy: ")
+        self.cnic_photocopy = input("\nEnter absolute path of cnic photocopy: ")
+        if (os.path.isfile(self.cnic_photocopy)):
+            if (not(self.cnic_photocopy.endswith((".jpeg", ".png")))):
+                print ("INVALID INPUT!\nFile format must be .jpeg or .png.")
+        else:
+            print ("INVALID PATH!\nFile does not exist.")
 
     def get_rating(self):
-        self.rating = input("Rate your experience (1 to 5) or Enter 0 to skip rating: ")
+        self.rating = input("\nRate your experience (1 to 5) or Enter 0 to skip rating: ")
         try: 
             self.rating = int(self.rating)
             if (self.rating != 0 and (self.rating < 1 or self.rating > 5)):
@@ -74,18 +80,19 @@ class HotelBookingApp:
     def display(self):
         print("\n----------INFO----------\n")
         print("Number of days: ", self.num_of_days)
-        print("Amount: ", self.amount)
+        print("Amount per day: ", self.amount)
         print("Name: ", self.name)
         print("Birthday: ", self.birthday)
         print("CNIC number: ", self.cnic)
         print("CNIC photocopy path: ", self.cnic_photocopy)
-        print("Rating: ", "Not rated yet" if self.rating == 0 else self.rating)
+        print("Rating: ", "Not rated yet" if self.rating == 0 else self.rating, "\n")
   
 # Driver code
 # Object instantiation
 h1 = HotelBookingApp()
   
 # Accessing class methods
+print("\n----------HOTEL BOOKING APP----------")
 h1.get_num_of_days()
 h1.get_amount()
 h1.get_user_info()
